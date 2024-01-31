24181190edd5bfa265bffa8e42c47863ef2c9dde
Gillian Lu <120929023+GillianLu@users.noreply.github.com>
2024-01-31 15:47:16 +0800

    Update main.sql

--- SQL/main.sql
+++ SQL/main.sql
@@ -1,7 +1,8 @@
 ACTIVITY_1
 CREATE DATABASE avionschool_development;
 
-CREATE TABLE students (
+CREATE TABLE students
+(
     id integer PRIMARY KEY,
     first_name varchar(255) NOT NULL,
     middle_name varchar(255) NULL,
@@ -10,9 +11,19 @@ CREATE TABLE students (
     location varchar(255)
 );
 
-INSERT INTO students (id, first_name, middle_name, last_name, age, location)
-VALUES (1, 'Gillian Nicole', 'Gonzales', 'Lu', 22, 'Philippines');
-VALUES (1, 'Gillian', 'Calayag', 'Akut', 22, 'Philippines');
+INSERT INTO students
+    (id, first_name, middle_name, last_name, age, location)
+VALUES
+    (1, 'Gillian Nicole', 'Gonzales', 'Lu', 22, 'Philippines'),
+    (2, 'Christia', 'Calayag', 'Akut', 22, 'Philippines'),
+    (3, 'Don', 'Alexander', 'Lim', 20, 'Philippines');
+
+UPDATE students
+SET first_name = 'Monica Gomos', City= 'Cebu'
+WHERE CustomerID = 3;
+
+DELETE FROM students WHERE first_name='Christia';
+
 
 
 
