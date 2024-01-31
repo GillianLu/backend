a45f5a35cf26d299feb1fce0234caa76da859c6b
Gillian Lu <120929023+GillianLu@users.noreply.github.com>
2024-01-31 16:10:16 +0800

    Update main.sql

--- SQL/main.sql
+++ SQL/main.sql
@@ -13,17 +13,36 @@ CREATE TABLE students
 
 INSERT INTO students
     (id, first_name, middle_name, last_name, age, location)
-VALUES
-    (1, 'Gillian Nicole', 'Gonzales', 'Lu', 22, 'Philippines'),
-    (2, 'Christia', 'Calayag', 'Akut', 22, 'Philippines'),
-    (3, 'Don', 'Alexander', 'Lim', 20, 'Philippines');
+VALUES(1, 'Gillian', 'Gonzales', 'Lu', 22, 'Manila'),
+    (2, 'John', 'Doe', 'Smith', 20, 'New York'),
+    (3, 'Jane', 'Alice', 'Brown', 19, 'Los Angeles'),
+    (4, 'Mike', 'Andrew', 'Johnson', 21, 'Chicago');
 
 UPDATE students
 SET first_name = 'Monica Gomos', City= 'Cebu'
-WHERE CustomerID = 3;
+WHERE id = 3;
 
 DELETE FROM students WHERE first_name='Christia';
 
+-- Activity 2 
+
+SELECT COUNT(first_name)
+FROM students
+WHERE location = 'Manila';
+
+-- • Select all students with location is Manila
+SELECT *
+FROM students
+WHERE location = 'Manila';
+
+-- • Display the average age of all students
+SELECT AVG(age)
+FROM students;
+
+-- • Display all students by age descending order
+SELECT *
+FROM students
+ORDER BY age DESC;
 
 
 
